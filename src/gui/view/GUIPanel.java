233adb95092efaba80f415b89ff6f50ebbb73e48
brandonjolley97 <brandonjolley97@gmail.com>
2015-11-23 08:55:06 -0700

    Implemented motionListener method
    Added all methods required for the MouseMotionListener interface.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -1,6 +1,7 @@
 package gui.view;
 
 import java.awt.Color;
+import java.awt.event.*;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing .JTextField;
@@ -27,13 +28,23 @@ public class GUIPanel extends JPanel
 		this.baseController = baseController;
 		
 		baseLayout = new SpringLayout();
-		firstButton = new JButton("Answer");
-		firstTextField = new JTextField("Question of Life");
+		firstButton = new JButton("Please don't click me!");
+		firstTextField = new JTextField("I have words....");
+		
 		
 		setupPanel();
 		setupLayout();
 		setupListeners();
+		changeRandomColor();
 		
+	}
+	
+	private void changeRandomColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		setBackground(new Color(red, green, blue));
 	}
 	
 	
@@ -48,12 +59,68 @@ public class GUIPanel extends JPanel
 	
 	private void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 67, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 165, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 0, SpringLayout.WEST, firstTextField);
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstButton, -47, SpringLayout.SOUTH, this);
 	}
 	
 	private void setupListeners()
 	{
+		this.addMouseListener(new MouseListener()
+		{
+			public void mouseClicked(MouseEvent click)
+			{
+				
+			}
+			
+			public void mouseReleased(MouseEvent released)
+			{
+				
+			}
+			
+			public void mousePressed(MouseEvent pressed)
+			{
+				
+			}
+			
+			public void mouseEntered(MouseEvent entered)
+			{
+				
+			}
+			
+			public void mouseExited(MouseEvent exited)
+			{
+				
+			}
+			
+		});
 		
+		this.addMouseMotionListener(new MouseMotionListener()
+		{
+			
+			public void mouseMoved(MouseEvent moved)
+			{
+				if(moved.isAltDown())
+				{
+					changeRandomColor();
+				}
+				
+				firstTextField.setText("Mouse X: " + moved.getX() + " Moved Y: " + moved.getY());
+				
+				if((Math.abs(moved.getX() - firstButton.getX()) < 5) && (Math.abs(moved.getY() - firstButton.getY()) < 5))
+				{
+					firstButton.setLocation((int) (Math.random() * 300), (int) (Math.random() * 300)); 
+					changeRandomColor();
+				}
+			}
+			
+			public void mouseDragged(MouseEvent dragged)
+			{
+				changeRandomColor();
+			}
+			
+		});
 	}
 	
 	
